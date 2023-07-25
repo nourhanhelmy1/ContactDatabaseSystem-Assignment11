9933638c8307ce8c3922be620ad23ecb0c550731
nourhanhelmy1 <117583711+nourhanhelmy1@users.noreply.github.com>
2023-07-26 01:37:23 +0300

    Added UPDATE query

--- ContactDatabase/ContactDatabase/Pages/Admin.cshtml.cs
+++ ContactDatabase/ContactDatabase/Pages/Admin.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Identity;
 using EdgeDB;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
 
 namespace ContactDatabase.Pages
 {
@@ -34,16 +35,57 @@ namespace ContactDatabase.Pages
             var passwordHasher = new PasswordHasher<string>();
             NewContact.Password = passwordHasher.HashPassword(null, NewContact.Password);
 
-            await _edgeDbClient.ExecuteAsync("INSERT Contact { first_name := <str>$firstName, last_name := <str>$lastName, email := <str>$email, title := <str>$title, description := <str>$description, date_of_birth := <datetime>$dateOfBirth, marriage_status := <bool>$marriageStatus, username := <str>$username, password := <str>$password, role := <str>$role }",
+            await _edgeDbClient.ExecuteAsync("INSERT Contact { first_name := <str>$first_name, last_name := <str>$last_name, email := <str>$email, title := <str>$title, description := <str>$description, date_of_birth := <datetime>date_of_birth, marriage_status := <bool>marriage_status, username := <str>$username, password := <str>$password, role := <str>$role }",
                 new Dictionary<string, object>
                 {
-                    { "firstName", NewContact.FirstName },
-                    { "lastName", NewContact.LastName },
+                    { "first_name", NewContact.FirstName },
+                    { "last_name", NewContact.LastName },
                     { "email", NewContact.Email },
                     { "title", NewContact.Title },
                     { "description", NewContact.Description },
-                    { "dateOfBirth", NewContact.DateOfBirth },
-                    { "marriageStatus", NewContact.MarriageStatus },
+                    { "date_of_birth", NewContact.DateOfBirth },
+                    { "marriage_status", NewContact.MarriageStatus },
+                    { "username", NewContact.Username },
+                    { "password", NewContact.Password },
+                    { "role", NewContact.Role },
+                });
+
+            return RedirectToPage();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> OnPostUpdateAsync([FromBody] JsonElement data)
+        {
+            var serializerOptions = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+            var contact = JsonSerializer.Deserialize<Contact>(data.GetRawText(), serializerOptions);
+
+            if (!ModelState.IsValid)
+            {
+                foreach (var modelState in ModelState.Values)
+                {
+                    foreach (var error in modelState.Errors)
+                    {
+                        Console.WriteLine(error.ErrorMessage);
+                    }
+                }
+
+                return BadRequest("Invalid data");
+            }
+
+            var query = "UPDATE Contact FILTER .username = <str>$Username SET { first_name := <str>$FirstName, last_name := <str>$LastName, email := <str>$Email, title := <str>$Title, description := <str>$Description, date_of_birth := <datetime>$DateOfBirth, marriage_status := <bool>$MarriageStatus, role := <str>$Role }";
+            await _edgeDbClient.ExecuteAsync(query, new Dictionary<string, object?>
+            {
+                    { "first_name", NewContact.FirstName },
+                    { "last_name", NewContact.LastName },
+                    { "email", NewContact.Email },
+                    { "title", NewContact.Title },
+                    { "description", NewContact.Description },
+                    { "date_of_birth", NewContact.DateOfBirth },
+                    { "marriage_status", NewContact.MarriageStatus },
                     { "username", NewContact.Username },
                     { "password", NewContact.Password },
                     { "role", NewContact.Role },
@@ -54,7 +96,7 @@ namespace ContactDatabase.Pages
 
         private async Task<List<Contact>> GetContactsAsync()
         {
-            var result = await _edgeDbClient.QueryAsync<Contact>("SELECT Contact {FirstName := .first_name, LastName := .last_name, Email := .email, Title := .title, Description := .description, DateOfBirth := .date_of_birth, MarriageStatus := .marriage_status }");
+            var result = await _edgeDbClient.QueryAsync<Contact>("SELECT Contact {FirstName := .first_name, LastName := .last_name, Email := .email, Title := .title, Description := .description, DateOfBirth := .date_of_birth, MarriageStatus := .marriage_status, Username := .username }");
             return result.ToList();
         }
     }
